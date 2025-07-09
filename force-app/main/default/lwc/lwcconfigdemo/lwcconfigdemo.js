3494ae079bf92d509416f65cd788c53c14137f44
rohini.t <rohini.t@autorabit.com>
2025-07-09 11:57:18 +0530

    LWC component config

---
+++ force-app/main/default/lwc/lwcconfigdemo/lwcconfigdemo.js
@@ -0,0 +1,10 @@
+import { LightningElement ,api } from 'lwc';
+
+export default class Lwcconfigdemo extends LightningElement {
+   
+    @api heading
+    @api recordId
+    @api age
+    @api levels
+
+}
